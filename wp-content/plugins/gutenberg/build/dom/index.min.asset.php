2ac2d9dcc88f5cebf8e173441b2b400797b883df
Bill D <bill@typhina.co.uk>
2022-12-29 11:38:44 +0000

    init

---
+++ wp-content/plugins/gutenberg/build/dom/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-deprecated', 'wp-polyfill'), 'version' => 'b0c484dc85037adee867');
---
+++ wp-content/plugins/gutenberg/build/element/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-escape-html', 'wp-polyfill'), 'version' => '7dbca47df69c8f2331a5');
